5587c7c2854e529ddddf0f6e93b729be29e98743
Nícolas Perez <nicolau.sobreira@gmail.com>
2025-01-12 13:43:47 -0300

    Add ...printmiddle

---
+++ src/graphics.c
@@ -0,0 +1,6 @@
+#include "graphics.h"
+
+void mvwprintmiddle(WINDOW *win, int y, int x, char *message) {
+    int middle_x = (int)x + my_strlen(message) / 2;
+    mvwprintw(win, y, middle_x, "%s", message);
+}
---
+++ src/graphics.h
@@ -0,0 +1,9 @@
+#include "utils.h"
+#include <ncurses.h>
+
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+void mvwprintmiddle(WINDOW *, int y, int x, char *);
+
+#endif
